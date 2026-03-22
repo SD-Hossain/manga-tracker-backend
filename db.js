1872628adcd889c5c67eb8f25ed7e78e37d50772
SD-Hossain <hossain501212@email.com>
2026-03-22 21:48:51 +0600

    fix: add production cors

--- db.js
+++ db.js
@@ -55,7 +55,9 @@ export const db = mysql.createPool({
   connectionLimit: 10,
   queueLimit: 0,
 
-  ssl: sslConfig
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 // ===============================
